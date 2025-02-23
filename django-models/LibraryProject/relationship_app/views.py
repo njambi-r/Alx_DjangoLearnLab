17e885420d69e7630ea5ba71bb97e035b220ea0a
Rose Njambi <njambi.r@gmail.com>
2025-02-23 13:34:13 +0300

    edits: added_login_logout_register templates, views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,11 +4,12 @@ from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
 #user registration
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 #user login
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render, redirect
 
 
 #1. Setting up template views
@@ -51,3 +52,20 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, "registration/signup.html", {"form": form})
+
+#login
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("book-list")  # Redirect to book list after login
+    else:
+        form = AuthenticationForm()
+    return render(request, "relationship_app/login.html", {"form": form})
+
+#logout
+def user_logout(request):
+    logout(request)
+    return render(request, "relationship_app/logout.html")
