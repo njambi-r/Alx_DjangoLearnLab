7f221bb6795cdc5dc85cbb131be14444e230675a
Rose Njambi <njambi.r@gmail.com>
2025-02-23 12:59:28 +0300

    views.py edit

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("books/", list_books, name="book-list"),  # Function-Based View
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library-book-detail"),  # Class-Based View
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'), #login
-    path('logout/', LogoutView.as_view(), name='logout'),#logout
+    path("logout/", LogoutView.as_view(template_name="registration/logout.html"), name="logout"),#logout
 ]
