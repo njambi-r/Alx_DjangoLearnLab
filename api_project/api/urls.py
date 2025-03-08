22ed2ef30ca1e055600e19eb907e1d5d384b572a
Rose Njambi <njambi.r@gmail.com>
2025-03-08 21:07:10 +0300

    Add querysets and routers

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from .views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename = 'book_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+
+    #include router urls for BookViewSet (all CRUD operations)
+    path('', include(router.urls)), #includes all routes registered with the router
 ]
