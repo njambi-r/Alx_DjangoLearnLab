cfb1d01a067ee0a709070cda2bd895a140115def
Rose Njambi <njambi.r@gmail.com>
2025-03-22 14:05:46 +0300

    Updates url patterns in blg/urls.py to match the checker format

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,9 +14,9 @@ urlpatterns = [
 # Connecting class-based views to allow CRUD actions
 from .views import PostListView, PostCreateView, PostDetailView, PostUpdateView, PostDeleteView
 urlpatterns += [
-    path("posts/", PostListView.as_view(), name="post-list"),
-    path("posts/new/", PostCreateView.as_view(), name="post-create"),
-    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post-edit"),
-    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+    path("post/", PostListView.as_view(), name="post-list"),
+    path("post/new/", PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
 ]
