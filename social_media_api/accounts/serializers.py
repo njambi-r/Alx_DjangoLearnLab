25c68a268682b341cd9d5c28e7b3d447e9fe1e68
Rose Njambi <njambi.r@gmail.com>
2025-03-28 14:19:55 +0300

    Edits accounts/serializers.py to match checker format

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -9,26 +9,29 @@ different user model, it is referenced using django.contrib.auth.get_user_model(
 This method will return the currently active user model - 
 the custom user model if one is specified, or User otherwise.
 """
-User = get_user_model()
+#User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['username','password','email','bio','profile_picture','followers']
-        extra_kwargs = {'password': {'write_only': True}} #This prevents the password from being included in API responses.
+        #extra_kwargs = {'password': {'write_only': True}} #This prevents the password from being included in API responses.
 
 # serializer for user registration
 class RegisterSerializer(serializers.ModelSerializer):
+    # Ensures that the password is treated as a string and is write-only (not included in responses)
+    password = serializers.CharField(write_only=True)
+
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['id', 'username', 'email', 'password'] #fields we allow users to provide when registering
-        extra_kwargs = {'password': {'write_only': True}} #prevents password from being included in API responses
 
     # method called when serializer.save() is executed
     # it receives validated_data -- which is cleaned up user input
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data) # Django’s built-in create_user() method to create a new user
-        token, created = Token.objects.get_or_create(user=user)
+        user = get_user_model().objects.create_user(**validated_data) # Django’s built-in create_user() method to create a new user
+        token, created = Token.objects.create(user=user)
         """
         If the user is newly created, a token is generated for them.
         If the user already exists, their existing token is retrieved.
