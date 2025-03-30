766430c9c4db2cb908153d50190c5df810563e05
Rose Njambi <njambi.r@gmail.com>
2025-03-30 19:30:45 +0300

    Explicitry add unlike route to posts/urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('', include(router.urls)),  # Includes all routes from the router
     path('feed/', UserFeedView.as_view(), name='user_feed'),
     path('<int:pk>/like/', LikePostView.as_view(), name='like_post'),
+    path('<int:pk>/unlike/', LikePostView.as_view(), name='unlike-post'),  # Explicit unlike route
 ]
 
